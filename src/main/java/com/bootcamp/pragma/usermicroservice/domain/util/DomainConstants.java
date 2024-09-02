b61284ec1b88faef927ca19e9cdf3273d6ece803
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-01 22:03:47 -0500

    feat(domain): add domain constants.

---
+++ src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/DomainConstants.java
@@ -0,0 +1,7 @@
+package com.bootcamp.pragma.usermicroservice.domain.util;
+
+public class DomainConstants {
+    private DomainConstants() {}
+
+    public static final String ROLE_ALREADY_EXISTS = "Role already exists";
+}
