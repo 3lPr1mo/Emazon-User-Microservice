6ae9e1e35c72284ae22aab188fcef77f51f11cfb
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-11 16:21:29 -0500

    feat(domain): add constants and custom exception.

--- src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/DomainConstants.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/DomainConstants.java
@@ -3,5 +3,5 @@ package com.bootcamp.pragma.usermicroservice.domain.util;
 public class DomainConstants {
     private DomainConstants() {}
 
-    public static final String ROLE_ALREADY_EXISTS = "Role already exists";
+    public static final String USER_ALREADY_EXISTS = "User already exists";
 }
