9a24dcf0f2aaee3c33964a8f5d3f15220d91b480
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:13:55 -0500

    feat(app): add login user method

--- src/main/java/com/bootcamp/pragma/usermicroservice/application/handler/AuthHandler.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/application/handler/AuthHandler.java
@@ -1,6 +1,8 @@
 package com.bootcamp.pragma.usermicroservice.application.handler;
 
+import com.bootcamp.pragma.usermicroservice.application.dto.request.LoginUser;
 import com.bootcamp.pragma.usermicroservice.application.dto.request.RegisterUser;
+import com.bootcamp.pragma.usermicroservice.application.dto.response.LoggedUserResponse;
 import com.bootcamp.pragma.usermicroservice.application.mapper.IAuthMapper;
 import com.bootcamp.pragma.usermicroservice.domain.api.IAuthServicePort;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,11 @@ public class AuthHandler {
 
     public void saveAuxUser(RegisterUser auxWarehouse) {
         authServicePort.registerAuxWarehouse(authMapper.toUserModel(auxWarehouse));
+    }
+
+    public LoggedUserResponse loginUser(LoginUser login) {
+        String token = authServicePort.login(login.getEmail(), login.getPassword());
+        return new LoggedUserResponse(token);
     }
 
 }
