8e8700f8d507a56d795704ccfcf4ab038b0273ca
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:00:03 -0500

    refract(entity): add @builder on user entity.

--- src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driven/jpa/mysql/entity/UserEntity.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driven/jpa/mysql/entity/UserEntity.java
@@ -1,10 +1,7 @@
 package com.bootcamp.pragma.usermicroservice.infrastructure.driven.jpa.mysql.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class UserEntity implements UserDetails {
 
     @Id
