8cc60f2f34f5447f3a2f236b554c24f394adc4cd
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:01:05 -0500

    refactor(util): add usernotfound & passwordnotmatch constants.

--- src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/DomainConstants.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/DomainConstants.java
@@ -4,4 +4,6 @@ public class DomainConstants {
     private DomainConstants() {}
 
     public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String USER_NOT_FOUND = "User Not Found";
+    public static final String PASSWORD_DOES_NOT_MATCH = "Password does not match";
 }
