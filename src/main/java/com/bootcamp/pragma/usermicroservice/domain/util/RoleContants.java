462d07075f2270d86aa3acee4819ea51e164d47e
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 15:42:31 -0500

    feat(domain): add roles constants

---
+++ src/main/java/com/bootcamp/pragma/usermicroservice/domain/util/RoleContants.java
@@ -0,0 +1,9 @@
+package com.bootcamp.pragma.usermicroservice.domain.util;
+
+public class RoleContants {
+    private RoleContants() {}
+
+    public static final String ROLE_ADMIN = "ADMINISTRADOR";
+    public static final String ROLE_CLIENT = "CLIENTE";
+    public static final String ROLE_AUX_WAREHOUSE = "AUX_BODEGA";
+}
