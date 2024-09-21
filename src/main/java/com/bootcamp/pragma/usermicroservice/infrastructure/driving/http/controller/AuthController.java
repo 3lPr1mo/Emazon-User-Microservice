af2117017ec42ada4cbddd22a6deb1da7faceb92
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:14:30 -0500

    feat(infrastructure): add post mapping to login user

--- src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driving/http/controller/AuthController.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driving/http/controller/AuthController.java
@@ -1,12 +1,15 @@
 package com.bootcamp.pragma.usermicroservice.infrastructure.driving.http.controller;
 
+import com.bootcamp.pragma.usermicroservice.application.dto.request.LoginUser;
 import com.bootcamp.pragma.usermicroservice.application.dto.request.RegisterUser;
+import com.bootcamp.pragma.usermicroservice.application.dto.response.LoggedUserResponse;
 import com.bootcamp.pragma.usermicroservice.application.handler.AuthHandler;
 import com.bootcamp.pragma.usermicroservice.infrastructure.driving.http.controller.util.SwaggerConstants;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +30,12 @@ public class AuthController {
     })
     public ResponseEntity<Void> registerAuxWarehouse(@Valid @RequestBody RegisterUser registerUser) {
         authHandler.saveAuxUser(registerUser);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<LoggedUserResponse> loginUser(@Valid @RequestBody LoginUser loginUser) {
+        return ResponseEntity.status(HttpStatus.OK).body(authHandler.loginUser(loginUser));
     }
 
 }
