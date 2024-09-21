340ee54f9b062efc6ea560b18db251967735f20c
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:05:52 -0500

    refactor(dto): add @email validation

--- src/main/java/com/bootcamp/pragma/usermicroservice/application/dto/request/RegisterUser.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/application/dto/request/RegisterUser.java
@@ -1,6 +1,7 @@
 package com.bootcamp.pragma.usermicroservice.application.dto.request;
 
 import com.bootcamp.pragma.usermicroservice.application.util.AuthConstants;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -20,6 +21,7 @@ public class RegisterUser {
     private String lastname;
 
     @NotBlank(message = AuthConstants.EMAIL_IS_REQUIRED_MSG)
+    @Email(message = AuthConstants.EMAIL_IS_NOT_VALID_MSG)
     private String email;
 
     @NotBlank(message = AuthConstants.PHONE_IS_REQUIRED_MSG)
