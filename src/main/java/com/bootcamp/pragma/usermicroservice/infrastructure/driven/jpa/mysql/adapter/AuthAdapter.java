594c22b622354ef439a17e6cec334dd364eece33
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-21 14:10:09 -0500

    feat(adapter): add login method and match password methods

--- src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driven/jpa/mysql/adapter/AuthAdapter.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/infrastructure/driven/jpa/mysql/adapter/AuthAdapter.java
@@ -1,6 +1,8 @@
 package com.bootcamp.pragma.usermicroservice.infrastructure.driven.jpa.mysql.adapter;
 
+import com.bootcamp.pragma.usermicroservice.domain.model.User;
 import com.bootcamp.pragma.usermicroservice.domain.spi.IAuthPersistencePort;
+import com.bootcamp.pragma.usermicroservice.infrastructure.driven.jpa.mysql.mapper.IUserEntityMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -8,9 +10,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AuthAdapter implements IAuthPersistencePort {
 
     private final PasswordEncoder passwordEncoder;
+    private final JwtAdapter jwtAdapter;
+    private final IUserEntityMapper userEntityMapper;
 
     @Override
     public String encodePassword(String password) {
         return passwordEncoder.encode(password);
+    }
+
+    @Override
+    public boolean matchPassword(String password, String encryptedPassword) {
+        return passwordEncoder.matches(password, encryptedPassword);
+    }
+
+    @Override
+    public String loginUser(User user) {
+        return jwtAdapter.generateToken(userEntityMapper.toEntityFromModel(user));
     }
 }
