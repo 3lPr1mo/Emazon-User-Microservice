01033ca4aa0525449d49cd84c65db8a1a5ab6dfa
3lPr1mo <erickenriquechaparro@gmail.com>
2024-09-28 20:11:20 -0500

    refactor(domain): add is adult method

--- src/main/java/com/bootcamp/pragma/usermicroservice/domain/api/usecase/UserUseCase.java
+++ src/main/java/com/bootcamp/pragma/usermicroservice/domain/api/usecase/UserUseCase.java
@@ -2,10 +2,14 @@ package com.bootcamp.pragma.usermicroservice.domain.api.usecase;
 
 import com.bootcamp.pragma.usermicroservice.domain.api.IUserServicePort;
 import com.bootcamp.pragma.usermicroservice.domain.exception.UserAlreadyExistException;
+import com.bootcamp.pragma.usermicroservice.domain.exception.UserIsUnderAgeException;
 import com.bootcamp.pragma.usermicroservice.domain.exception.UserNotExistException;
 import com.bootcamp.pragma.usermicroservice.domain.model.User;
 import com.bootcamp.pragma.usermicroservice.domain.spi.IUserPersistencePort;
 import com.bootcamp.pragma.usermicroservice.domain.util.DomainConstants;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class UserUseCase implements IUserServicePort {
 
@@ -23,11 +27,22 @@ public class UserUseCase implements IUserServicePort {
         if(userPersistencePort.findByEmail(user.getEmail()).isPresent()){
             throw new UserAlreadyExistException(DomainConstants.USER_ALREADY_EXISTS + user.getEmail());
         }
+        if(!isAdult(user.getDateBirth())){
+            throw new UserIsUnderAgeException(DomainConstants.USER_IS_UNDER_AGE);
+        }
         userPersistencePort.save(user);
     }
 
     @Override
     public User findUserByEmail(String email) {
         return userPersistencePort.findByEmail(email).orElseThrow(() -> new UserNotExistException(DomainConstants.USER_NOT_FOUND));
+    }
+
+    private boolean isAdult(Date date) {
+        Calendar calendarBirth = Calendar.getInstance();
+        calendarBirth.setTime(date);
+        Calendar calendarNow = Calendar.getInstance();
+        calendarNow.setTime(new Date());
+        return calendarNow.get(Calendar.YEAR) - calendarBirth.get(Calendar.YEAR) >= 18;
     }
 }
